e1f47a9eb4c1f92f96be4d6aac2827c87bcd4a0c
Tekleab15 <teklishgmn15@gmail.com>
2025-03-25 12:31:59 +0300

    comming back to the basic test

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -1,4 +1,5 @@
-test('Basic Test', () => {
-    expect(1 + 1).toBe(2);
+describe('Backend Functionality', () => {
+  test('should return true to indicate that the test environment is set up', () => {
+    expect(true).toBe(true);
   });
-  
+});
